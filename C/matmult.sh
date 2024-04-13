5aa9669bd45a5edbdad416589666db0de843f7dd
jakeheber201 <hebertj@etsu.edu>
2024-04-13 00:36:04 -0400

    CUDA samples work, time to make some benchmarks!

---
+++ C/matmult.sh
@@ -0,0 +1,20 @@
+#!/bin/bash 
+
+
+#if args != 1
+if [ "$#" -ne 1 ]; then
+  ech "Usage: $0 MATRIX_SIZE"
+  exit 1
+fi 
+
+#Sets MATRIX_SIZE to first arg 
+MATRIX_SIZE="$1"
+
+
+#new line 
+echo "" >> results.txt
+
+#Runs with args
+#I want to only show MatrixA and Performance= lines
+./matrixMul -wA=$MATRIX_SIZE -hA=$MATRIX_SIZE -wB=$MATRIX_SIZE -hB=$MATRIX_SIZE | grep -E "MatrixA|Performance"   >> results.txt
+
